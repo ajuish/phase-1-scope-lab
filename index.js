527b5a8f52066e80aefd10e4cc3b5f8a3e2c8b36
Alex Ju <aju217@gmail.com>
2022-09-20 23:21:22 -0400

    commit tree 66bf122d2f4b97e11f56edd0472f2b6af287a4ff

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName =  customerName.toUpperCase();    
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "mack"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "james";
+    return leastFavoriteCustomer;
+}
+// setBestCustomer();
+// overwriteBestCustomer();
+// console.log(bestCustomer)
+// changeLeastFavoriteCustomer();
+// console.log(leastFavoriteCustomer)
